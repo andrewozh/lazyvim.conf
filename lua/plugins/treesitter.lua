70a7afbd69a6c99f54077fb96f4c7c021b8816fc
andrewozh <andrew.ozhegov@gmail.com>
2025-05-06 11:52:53 +0300

    treesitter cfg

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "terraform",
+        "helm",
+      },
+    },
+  },
+}
